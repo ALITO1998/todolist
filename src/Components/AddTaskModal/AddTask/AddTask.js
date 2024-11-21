446006144c864ec38b9d92f1d0f52e21f1870539
ALITO1998 <alialaa144144@gmail.com>
2024-11-22 02:15:22 +0300

    remove unused code & add late icon to task

--- src/Components/AddTaskModal/AddTask/AddTask.js
+++ src/Components/AddTaskModal/AddTask/AddTask.js
@@ -1,18 +1,19 @@
 import React from 'react';
-import { Box, FormControl, IconButton, Alert } from '@mui/material';
+import { Box, FormControl, IconButton, Alert, FormLabel } from '@mui/material';
 import Task from '../../Content/Task/Task';
 
 const AddTask = ({ addTaskBtnHandler, alertEmpty, inputTaskHandler, task }) => {
 
     return (
         <Box id='addTaskBox' component="form" onSubmit={addTaskBtnHandler} sx={{ margin: '10px', }}>
-            <FormControl sx={{ marginRight: '25px', display: 'flex' }} >
+            <FormLabel sx={{ fontSize: '32px' }}>Task</FormLabel>
+            <FormControl sx={{ margin: '10px', display: 'flex' }} >
                 {alertEmpty && <Alert severity="error" sx={{ mb: '5px' }}>Task cannot be empty</Alert>}
             </FormControl>
 
             <Task type='add' inputTaskHandler={inputTaskHandler} task={task} />
 
-            <FormControl>
+            <FormControl sx={{ margin: '25px', display: 'flex' }}>
                 <IconButton type="submit" >Save</IconButton>
             </FormControl>
         </Box >);
--- src/Components/Content/Task/Task.js
+++ src/Components/Content/Task/Task.js
@@ -1,10 +1,9 @@
-import { FormControl, Input, InputLabel, ListItemText, Typography } from '@mui/material'
+import { Alert, FormControl, Input, InputLabel, ListItemText } from '@mui/material'
 import React, { Fragment } from 'react'
-import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import dayjs from 'dayjs';
-import { DateField, DatePicker } from '@mui/x-date-pickers';
+import { DatePicker } from '@mui/x-date-pickers';
 
 const Task = ({ task, type, inputTaskHandler }) => {
     if (type === 'add') {
@@ -16,23 +15,23 @@ const Task = ({ task, type, inputTaskHandler }) => {
 
 const TaskShow = ({ task }) => {
     const date = new Date(task.expectedDoneDate.$d);
-    return <Fragment>
+    return (<Fragment>
         <ListItemText
             primary={task.title}
             secondary={"Task end date: " + date.getFullYear() + "/" + (date.getMonth() + 1) + "/" + date.getDate()}
             sx={task.status === 'done' ? { textDecorationLine: 'line-through' } : {}} />
-    </Fragment>
+        {task.status === 'not yet' && date < new Date() ? <Alert severity="warning">Late</Alert> : <></>}
+    </Fragment>);
 }
 
 
 const TaskAdded = ({ inputTaskHandler, task }) => {
     return (<Fragment>
-        <FormControl sx={{ marginRight: '25px' }} >
+        <FormControl sx={{ margin: '25px', display: 'flex', marginTop: "0px" }} >
             <InputLabel sx={{ fontSize: '28px' }} htmlFor="title">Title</InputLabel>
             <Input id="title" value={task.title || ""} onChange={inputTaskHandler} placeholder='Enter Your Task Title...' />
         </FormControl>
-        <FormControl>
-            {/* <Typography sx={{ fontSize: '28px', padding: '5px' }} htmlFor="expectedDoneDate">Expected Done Date</Typography> */}
+        <FormControl sx={{ margin: '25px', display: 'flex' }}>
             <LocalizationProvider dateAdapter={AdapterDayjs}>
                 <DatePicker label='Expected Done Date' id='expectedDoneDate' value={task.expectedDoneDate || dayjs()} onChange={(e) => { inputTaskHandler(e) }} />
             </LocalizationProvider>
