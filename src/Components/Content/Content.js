fc62715f9c8d9c62f3521a66a120731335d3deb4
ALITO1998 <alialaa144144@gmail.com>
2025-01-08 15:41:09 +0300

    optimize content component (turn shownData to Fn)

--- src/Components/Content/Content.js
+++ src/Components/Content/Content.js
@@ -3,14 +3,12 @@ import NavigationStatus from './NavigationStatus/NavigationStatus'
 import AddTaskBtn from './AddTaskBtn/AddTaskBtn'
 import { Box } from '@mui/material'
 import TaskList from './TaskList/TaskList'
-//import dayjs from 'dayjs'
 import AddTaskModal from '../AddTaskModal/AddTaskModal'
 import { useDispatch, useSelector } from 'react-redux'
 import { getTasks, addTask, deleteTask, updateTask } from '../../store/taskSlice'
 const Content = () => {
     const [status, setStatus] = useState('all');
     const [showModal, setShowModal] = useState(false);
-    const [shownData, setShownData] = useState([]);
     const { tasks } = useSelector((state) => state.tasks);
     const dispatch = useDispatch();
 
@@ -23,17 +21,14 @@ const Content = () => {
         dispatch(getTasks());
     }, [dispatch])
 
-
-    useEffect(() => {
+    const shownData = () => {
+        if (!tasks) return [];
         if (status === "all") {
-            setShownData(tasks);
+            return tasks;
         } else {
-            setShownData(tasks.filter((ele) => ele.task.status === status));
+            return tasks.filter((ele) => ele.task.status === status);
         }
-
-    }, [dispatch, status, tasks]);
-
-
+    }
 
     const openModal = () => {
         setShowModal(true);
@@ -50,7 +45,7 @@ const Content = () => {
                 <NavigationStatus status={status} handleChangeStatus={handleChangeStatus} />
 
                 <TaskList
-                    data={shownData}
+                    data={shownData()}
                     handleDelete={(id) => { dispatch(deleteTask(id)) }}
                     handleChecked={(item) => {
                         dispatch(updateTask({ id: item.id, task: { ...item.task, status: item.task.status === "not yet" ? "done" : "not yet" } }))
